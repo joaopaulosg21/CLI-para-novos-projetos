fce212b4d3803f99967b544f268436d4b40d0256
joaopaulo <joaopaulosg17@gmail.com>
2022-03-16 19:55:23 -0300

    Adicionada função para alterar onde o projeto será criado

--- index.js
+++ index.js
@@ -7,32 +7,38 @@ const read = readline.createInterface({
     output:process.stdout
 })
 
-read.question('Digite o nome do projeto: ',(folderName)=>{
-    try{
-        const atualPath = process.cwd();
-        fs.mkdirSync(folderName)
-        process.chdir(atualPath + '/' + folderName)
-        fs.writeFile('server.js','teste',err =>{
-            if(err){
-                console.log(err)
-                return
+read.question('Digite o nome do projeto ',(folderName)=>{
+    if(folderName){
+        read.question('Digite o caminho onde será criado: ',(folder)=>{
+            try{
+                process.chdir(folder)
+                const atualPath = process.cwd();
+                fs.mkdirSync(folderName)
+                process.chdir(atualPath + '/' + folderName)
+                fs.writeFile('server.js','teste',err =>{
+                    if(err){
+                        console.log(err)
+                        return
+                    }
+                })
+                fs.mkdirSync('database')
+                process.chdir(atualPath + '/' + folderName + '/' + 'database')
+                fs.writeFile('database.js','teste',err =>{
+                    if(err){
+                        console.log(err)
+                        return
+                    }
+                })
+                fs.mkdirSync('models')
+                process.chdir(atualPath + '/' + folderName)
+                fs.mkdirSync('controllers')
+                console.log('Projeto criado')
+                read.close()
+            }catch(error){
+                console.log(error)
+                read.close()
             }
         })
-        fs.mkdirSync('database')
-        process.chdir(atualPath + '/' + folderName + '/' + 'database')
-        fs.writeFile('database.js','teste',err =>{
-            if(err){
-                console.log(err)
-                return
-            }
-        })
-        fs.mkdirSync('models')
-        process.chdir(atualPath + '/' + folderName)
-        fs.mkdirSync('controllers')
-        console.log('Projeto criado')
-        read.close()
-    }catch(error){
-        console.log(error)
-        read.close()
     }
+    
 })
