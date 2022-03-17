5cabb8c40dad3adf1be10458989f673f728db21a
joaopaulo <joaopaulosg17@gmail.com>
2022-03-17 10:21:34 -0300

    Adicionada criação da pasta routes

--- index.js
+++ index.js
@@ -13,25 +13,44 @@ read.question('Digite o nome do projeto ',(folderName)=>{
             try{
                 process.chdir(folder)
                 const atualPath = process.cwd();
+
+                //Pasta do projeto
                 fs.mkdirSync(folderName)
                 process.chdir(atualPath + '/' + folderName)
-                fs.writeFile('server.js','teste',err =>{
+                fs.writeFile('server.js','',err =>{
                     if(err){
                         console.log(err)
                         return
                     }
                 })
+
+                //Pasta database
                 fs.mkdirSync('database')
                 process.chdir(atualPath + '/' + folderName + '/' + 'database')
-                fs.writeFile('database.js','teste',err =>{
+                fs.writeFile('database.js','',err =>{
                     if(err){
                         console.log(err)
                         return
                     }
                 })
+                
+                //Pasta models
                 fs.mkdirSync('models')
                 process.chdir(atualPath + '/' + folderName)
+
+                //Pasta controllers
                 fs.mkdirSync('controllers')
+                process.chdir(atualPath + '/' + folderName)
+
+                //Pasta routes
+                fs.mkdirSync('routes')
+                process.chdir(atualPath + '/' + folderName + '/' + 'routes')
+                fs.writeFile('routes.js','',err => {
+                    if(err){
+                        console.log(err)
+                        return
+                    }
+                })
                 console.log('Projeto criado')
                 read.close()
             }catch(error){
